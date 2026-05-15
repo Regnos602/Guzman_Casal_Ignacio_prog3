c93882694ff4327f7efd6975e7f9b86f01b0f311
Regnos602 <nachoguzmancasal@live.com.ar>
2026-05-15 20:46:46 -0300

    Puntos 9 y 10

--- guardados.js
+++ guardados.js
@@ -1,11 +1,14 @@
 const contenedorCartas = document.querySelector("#cartas");
 
-function mostrarCartasGuardadas() {
-  const cartasGuardadas = JSON.parse(localStorage.getItem("cartas")) || [];
+const botonOrdenarValor = document.querySelector("#ordenarValor");
+const botonOrdenarPalo = document.querySelector("#ordenarPalo");
 
+let cartasGuardadas = JSON.parse(localStorage.getItem("cartas")) || [];
+
+function mostrarCartasGuardadas(cartas) {
   contenedorCartas.innerHTML = "";
 
-  cartasGuardadas.forEach(cartaGuardada => {
+  cartas.forEach(cartaGuardada => {
     const carta = new Carta(
       cartaGuardada.code,
       cartaGuardada.value,
@@ -20,4 +23,23 @@ function mostrarCartasGuardadas() {
   });
 }
 
-mostrarCartasGuardadas();
+function ordenarPorValor() {
+  cartasGuardadas.sort((a, b) => {
+    return a.value.localeCompare(b.value);
+  });
+
+  mostrarCartasGuardadas(cartasGuardadas);
+}
+
+function ordenarPorPalo() {
+  cartasGuardadas.sort((a, b) => {
+    return a.suit.localeCompare(b.suit);
+  });
+
+  mostrarCartasGuardadas(cartasGuardadas);
+}
+
+botonOrdenarValor.addEventListener("click", ordenarPorValor);
+botonOrdenarPalo.addEventListener("click", ordenarPorPalo);
+
+mostrarCartasGuardadas(cartasGuardadas);
